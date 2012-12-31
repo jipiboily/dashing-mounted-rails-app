599c1f6b0a70dc2d4b2c8e6ce1678a9a07f57242
Jean-Philippe Boily <j@jipi.ca>
2012-12-31 12:05:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DashingMountedRailsApp::Application.config.secret_token = '4c8c355d7b957f84dac4122f53c7d23188a34b400c6a9612e82684c94c32a67d9078fb34bb30c494a40fe4c649b3cf211d5d206d8717b66e74966d808ea92257'
